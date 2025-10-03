d076e5f2a94764dedc2d23546efce100df6144af
Hever Gonzalez <hevergonzalez@WS-J3MFQPLQR7-CHA.local>
2025-10-02 19:50:50 -0600

    add class doctor

---
+++ tecmilenio-consultapp/src/main/java/com/tecmilenio/tecmilenio/consultapp/TecmilenioConsultapp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.tecmilenio.tecmilenio.consultapp;
+
+/**
+ *
+ * @author hevergonzalez
+ */
+public class TecmilenioConsultapp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
